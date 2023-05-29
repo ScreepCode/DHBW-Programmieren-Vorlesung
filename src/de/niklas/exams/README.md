31062861f3b15b85e74d92614ccb30f38fddeb37
Niklas Buse <nicki_bu@gmx.de>
2023-05-29 21:36:58 +0200

    New: Probeklausur

---
+++ src/de/niklas/exams/README.md
@@ -0,0 +1,21 @@
+# Bearbeitete Probeklausuren
+
+> ## Disclaimer!!!
+> Diese Aufgaben haben keine Garantie auf Vollständigkeit oder Richtigkeit! <br>
+> Ich gebe mein bestes aber es können Fehler enthalten sein. <br>
+> Die ausführlichen Aufgabenstellungen findet ihr auf anderen Wegen ^^
+> ## Wichtig: 
+> Setzte das Working-Directory, damit es richtig ausgeführt werden kann, sonst werden die Lokalen Dateien nicht gefunden
+
+> ### Bisher bearbeitete Klausuren:
+> - [Probeklausur 2023](#content_01)
+
+## 📗 Dateiauflistung <a name="content"></a>
+
+### Probeklausur 2023 <a name="content_01"></a>
+> - [DartsCounter](test_exam2023/DartsCounter.java)
+> - [Field](test_exam2023/Field.java)
+> - [Board](test_exam2023/Board.java)
+> - [Visit](test_exam2023/Visit.java)
+> - [Player](test_exam2023/Player.java)
+> - [Game](test_exam2023/Game.java)
---
+++ src/de/niklas/exams/test_exam2023/Player.java
@@ -0,0 +1,60 @@
+package de.niklas.exams.test_exam2023;
+
+/**
+ * <strong>Player</strong><br>
+ * Die Implementation eines Spielers
+ *
+ * @see "Teilaufgabe d & g"
+ * @author Niklas Buse
+ */
+public class Player {
+
+    private String name;
+
+    private int countDartsThrown;
+    private Visit[] visits = new Visit[10];
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCountDartsThrown() {
+        return countDartsThrown;
+    }
+
+    public int getRemainingPoints(){
+        int remainingPoints = 501;
+        for(Visit visit : visits){
+            if(visit != null){
+                remainingPoints -= visit.getValue();
+            }
+        }
+        return remainingPoints;
+    }
+
+    public boolean addVisit(Visit visit){
+        if(getRemainingPoints()-visit.getValue() == 0 & visit.getLastField().isDoubleField()){
+            visits[countDartsThrown] = visit;
+            countDartsThrown++;
+            return true;
+        }
+        if(getRemainingPoints()-visit.getValue() <= 1){
+            countDartsThrown++;
+            return false;
+        }
+        else{
+            visits[countDartsThrown] = visit;
+            countDartsThrown++;
+            return true;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Player: %s, %d points remaining.", getName(), getRemainingPoints());
+    }
+}
